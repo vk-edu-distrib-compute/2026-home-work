e064f78efbff0163441dbd99facb06e931f655c0
d1gitale <nkalinin2020@gmail.com>
2026-04-01 18:27:22 +0300

    fix: undo changes to Server.java

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,19 +1,15 @@
 package company.vk.edu.distrib.compute;
 
-import java.io.IOException;
-import company.vk.edu.distrib.compute.d1gitale.KVServiceFactoryImpl;
+import module java.base;
+import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
 import org.slf4j.LoggerFactory;
 
-public final class Server {
+public class Server {
 
-    private Server() {
-        // Utility class
-    }
-
-    public static void main(String[] args) throws IOException {
+    void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new KVServiceFactoryImpl().create(port);
+        KVService storage = new DummyKVServiceFactory().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
