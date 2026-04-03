622eaa672568b918ba8f3eb7ae26d8d1230fd1e7
tadzhnahal <tadzhnahal@yahoo.com>
2026-04-03 14:14:19 +0300

    add service start stop and status

--- src/main/java/company/vk/edu/distrib/compute/tadzhnahal/TadzhnahalKVService.java
+++ src/main/java/company/vk/edu/distrib/compute/tadzhnahal/TadzhnahalKVService.java
@@ -1,6 +1,7 @@
 package company.vk.edu.distrib.compute.tadzhnahal;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
 import company.vk.edu.distrib.compute.Dao;
 import company.vk.edu.distrib.compute.KVService;
 
@@ -19,11 +20,49 @@ public class TadzhnahalKVService implements KVService {
 
     @Override
     public void start() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (server != null) {
+            throw new IllegalStateException("Server already started");
+        }
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/v0/status", this::handleStatus);
+            server.start();
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot start server", e);
+        }
     }
 
     @Override
     public void stop() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (server == null) {
+            throw new IllegalStateException("Sever is not started");
+        }
+
+        server.stop(0);
+        server = null;
+
+        try {
+            dao.close();
+        }  catch (IOException e) {
+            throw new IllegalStateException("Cannot close dao", e);
+        }
+    }
+
+    private void handleStatus(HttpExchange exchange) throws IOException {
+        try {
+            if (!"GET".equals(exchange.getRequestMethod())) {
+                sendEmptyResponse(exchange, 405);
+                return;
+            }
+
+            sendEmptyResponse(exchange, 200);
+        } finally {
+            exchange.close();
+        }
+    }
+
+    private void sendEmptyResponse(HttpExchange exchange, int code) throws IOException {
+        exchange.sendResponseHeaders(code, -1);
     }
 }
