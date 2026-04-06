c9abf72288c6ffc6281cd310584bc71100208e7d
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2026-04-06 03:00:28 +0300

    Add FileDao (bonus task 1)

--- src/main/java/company/vk/edu/distrib/compute/martinez1337/service/DefaultKVServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/martinez1337/service/DefaultKVServiceFactory.java
@@ -2,14 +2,15 @@ package company.vk.edu.distrib.compute.martinez1337.service;
 
 import company.vk.edu.distrib.compute.KVService;
 import company.vk.edu.distrib.compute.KVServiceFactory;
-import company.vk.edu.distrib.compute.martinez1337.dao.InMemoryDao;
+import company.vk.edu.distrib.compute.martinez1337.dao.FileDao;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
-public class InMemoryKVServiceFactory extends KVServiceFactory {
+public class DefaultKVServiceFactory extends KVServiceFactory {
 
     @Override
     protected KVService doCreate(int port) throws IOException {
-        return new InMemoryKVService(port, new InMemoryDao());
+        return new DefaultKVService(port, new FileDao(Path.of(".kv_data")));
     }
 }
