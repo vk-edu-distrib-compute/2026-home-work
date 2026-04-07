b6af45a1bebb0e8f21967608b5228c35d87e0b91
KuznetsovaSvetlana6 <kuznetsovasvetlana.006@yandex.ru>
2026-04-07 20:41:15 +0300

    Delete put.lua

--- put.lua
+++
@@ -1,6 +0,0 @@
-request = function()
-    local id = tostring(math.random(1, 5000))
-    local path = "/v0/entity?id=" .. id
-    local body = "data_" .. id
-    return wrk.format("PUT", path, nil, body)
-end
