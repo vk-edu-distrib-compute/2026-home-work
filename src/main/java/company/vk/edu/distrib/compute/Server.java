a8772da21ecc3b882defb4447d39caf3370f7dd1
KuznetsovaSvetlana6 <kuznetsovasvetlana.006@yandex.ru>
2026-04-07 20:39:56 +0300

    Update Server.java

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,14 +1,15 @@
 package company.vk.edu.distrib.compute;
 
 import module java.base;
+import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
 import org.slf4j.LoggerFactory;
-import company.vk.edu.distrib.compute.kuznetsovasvetlana6.MyKVServiceFactoryFile;
 
 public class Server {
+
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new MyKVServiceFactoryFile().create(port);
+        KVService storage = new DummyKVServiceFactory().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
