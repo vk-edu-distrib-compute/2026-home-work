4aea4083202f64e5e751663467d9ec521d9dd88e
Vitaly Egorov <vitaly-egorow21@yandex.ru>
2026-04-08 22:35:00 +0300

    Switch to ConsistentHashingShardResolver

--- src/main/java/company/vk/edu/distrib/compute/vitos23/Vitos23KVClusterFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/vitos23/Vitos23KVClusterFactory.java
@@ -2,7 +2,7 @@ package company.vk.edu.distrib.compute.vitos23;
 
 import company.vk.edu.distrib.compute.KVCluster;
 import company.vk.edu.distrib.compute.KVClusterFactory;
-import company.vk.edu.distrib.compute.vitos23.shard.RendezvousShardResolver;
+import company.vk.edu.distrib.compute.vitos23.shard.ConsistentHashingShardResolver;
 import company.vk.edu.distrib.compute.vitos23.shard.ShardResolver;
 import company.vk.edu.distrib.compute.vitos23.shard.ShardedKVServiceFactory;
 import company.vk.edu.distrib.compute.vitos23.util.HttpUtils;
@@ -14,8 +14,8 @@ public class Vitos23KVClusterFactory extends KVClusterFactory {
     protected KVCluster doCreate(List<Integer> ports) {
         List<String> shards = ports.stream().map(HttpUtils::getLocalEndpoint).toList();
         // ATTENTION: sharding algorithm choice
-        ShardResolver shardResolver = new RendezvousShardResolver(shards);
-        // ShardResolver shardResolver = new ConsistentHashingShardResolver(shards);
+        // ShardResolver shardResolver = new RendezvousShardResolver(shards);
+        ShardResolver shardResolver = new ConsistentHashingShardResolver(shards);
         ShardedKVServiceFactory shardedKVServiceFactory = new ShardedKVServiceFactory(shardResolver);
         return new KVClusterImpl(shardedKVServiceFactory, ports);
     }
