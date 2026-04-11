57dc7663b93f682cb97e5302bda895a62a837e99
Artsobol <artsobol05@yandex.ru>
2026-04-11 20:23:41 +0300

    fix: import

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,15 +1,16 @@
 package company.vk.edu.distrib.compute;
 
-import module java.base;
-import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
+import company.vk.edu.distrib.compute.artsobol.ConcreteKVServiceFactory;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class Server {
 
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new DummyKVServiceFactory().create(port);
+        KVService storage = new ConcreteKVServiceFactory().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
