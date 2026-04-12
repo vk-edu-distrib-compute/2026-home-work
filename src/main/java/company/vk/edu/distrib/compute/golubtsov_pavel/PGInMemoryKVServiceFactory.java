48cee3150a1bc4ee5c85b0c26ef195eff09f68e8
Chessman2003 <145466841+Chessman2003@users.noreply.github.com>
2026-04-12 22:10:27 +0300

    fix: codeStyle fix in PGInMemoryKVServiceFactory

--- src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
@@ -3,7 +3,8 @@ package company.vk.edu.distrib.compute.golubtsov_pavel;
 import company.vk.edu.distrib.compute.KVService;
 import company.vk.edu.distrib.compute.KVServiceFactory;
 
-import java.io.IOException;import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class PGInMemoryKVServiceFactory extends KVServiceFactory {
     @Override
