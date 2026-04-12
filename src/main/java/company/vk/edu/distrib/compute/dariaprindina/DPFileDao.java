54943394df3cc002b88eb781d109a2b89d68d153
Daria Prindina <daria@prindina.ru>
2026-04-12 12:56:29 +0300

    Fix synchronization in Persistent Dao

--- src/main/java/company/vk/edu/distrib/compute/dariaprindina/DPFileDao.java
+++ src/main/java/company/vk/edu/distrib/compute/dariaprindina/DPFileDao.java
@@ -10,11 +10,13 @@ import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DPFileDao implements Dao<byte[]> {
     private static final Base64.Encoder BASE64_URL = Base64.getUrlEncoder().withoutPadding();
 
     private final Path rootDir;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public DPFileDao(Path rootDir) throws IOException {
         this.rootDir = rootDir;
@@ -22,33 +24,48 @@ public class DPFileDao implements Dao<byte[]> {
     }
 
     @Override
-    public synchronized byte[] get(String key) throws NoSuchElementException, IllegalArgumentException, IOException {
+    public byte[] get(String key) throws NoSuchElementException, IllegalArgumentException, IOException {
         validateKey(key);
-        final Path keyPath = pathForKey(key);
-        if (!Files.exists(keyPath)) {
-            throw new NoSuchElementException("no value for key " + key);
+        lock.lock();
+        try {
+            final Path keyPath = pathForKey(key);
+            if (!Files.exists(keyPath)) {
+                throw new NoSuchElementException("no value for key " + key);
+            }
+            return Files.readAllBytes(keyPath);
+        } finally {
+            lock.unlock();
         }
-        return Files.readAllBytes(keyPath);
     }
 
     @Override
-    public synchronized void upsert(String key, byte[] value) throws IllegalArgumentException, IOException {
+    public void upsert(String key, byte[] value) throws IllegalArgumentException, IOException {
         validateKey(key);
         if (value == null) {
             throw new IllegalArgumentException("value is null");
         }
-        final Path keyPath = pathForKey(key);
-        final Path tmpPath = rootDir.resolve(
-            "tmp-" + ThreadLocalRandom.current().nextInt(1_000_000) + ".bin"
-        );
-        Files.write(tmpPath, value);
-        Files.move(tmpPath, keyPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
+        lock.lock();
+        try {
+            final Path keyPath = pathForKey(key);
+            final Path tmpPath = rootDir.resolve(
+                "tmp-" + ThreadLocalRandom.current().nextInt(1_000_000) + ".bin"
+            );
+            Files.write(tmpPath, value);
+            Files.move(tmpPath, keyPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
-    public synchronized void delete(String key) throws IllegalArgumentException, IOException {
+    public void delete(String key) throws IllegalArgumentException, IOException {
         validateKey(key);
-        Files.deleteIfExists(pathForKey(key));
+        lock.lock();
+        try {
+            Files.deleteIfExists(pathForKey(key));
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
