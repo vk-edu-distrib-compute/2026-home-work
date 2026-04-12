e12b68309649471e6539118d24fecc5fb8957f79
Nastyasolnyyt <116307870+Nastyasolnyyt@users.noreply.github.com>
2026-04-12 14:50:05 +0300

    Update SolntsevaKVClusterFactory.java

--- src/main/java/company/vk/edu/distrib/compute/solntseva_nastya/SolntsevaKVClusterFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/solntseva_nastya/SolntsevaKVClusterFactory.java
@@ -4,6 +4,7 @@ import company.vk.edu.distrib.compute.KVCluster;
 import company.vk.edu.distrib.compute.KVClusterFactory;
 import company.vk.edu.distrib.compute.KVService;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,26 +14,29 @@ import java.util.stream.Collectors;
 public class SolntsevaKVClusterFactory extends KVClusterFactory {
     @Override
     public KVCluster doCreate(List<Integer> ports) {
+        List<String> endpoints = ports.stream()
+                .map(port -> "http://localhost:" + port)
+                .toList();
+
+        Set<String> topology = endpoints.stream().collect(Collectors.toSet());
+        List<KVService> services = new ArrayList<>(ports.size());
+
         try {
-            List<String> endpoints = ports.stream()
-                    .map(port -> "http://localhost:" + port)
-                    .toList();
-
-            Set<String> topology = endpoints.stream().collect(Collectors.toSet());
-
-            List<KVService> services = new ArrayList<>();
             for (int port : ports) {
                 String myUrl = "http://localhost:" + port;
+                Path storagePath = Paths.get("storage", "data_" + port);
+                
                 services.add(new SolntsevaKVService(
                         port,
-                        new PersistentDao(Paths.get("storage", "data_" + port)),
+                        new PersistentDao(storagePath),
                         topology,
                         myUrl
                 ));
             }
-            return new SolntsevaKVCluster(services, endpoints);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to create cluster nodes", e);
+            throw new IllegalStateException("Failed to initialize cluster storage nodes", e);
         }
+
+        return new SolntsevaKVCluster(services, endpoints);
     }
 }
