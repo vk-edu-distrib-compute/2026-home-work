24f1d90a09e6c90b5f2cc8c7877330a460a55d4a
Chessman2003 <145466841+Chessman2003@users.noreply.github.com>
2026-04-12 21:13:47 +0300

    first task: File storage Dao

--- src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
@@ -3,11 +3,11 @@ package company.vk.edu.distrib.compute.golubtsov_pavel;
 import company.vk.edu.distrib.compute.KVService;
 import company.vk.edu.distrib.compute.KVServiceFactory;
 
-import java.io.IOException;
+import java.io.IOException;import java.nio.file.Path;
 
 public class PGInMemoryKVServiceFactory extends KVServiceFactory {
     @Override
     protected KVService doCreate(int port) throws IOException {
-        return new PGInMemoryKVService(port, new PGInMemoryDao());
+        return new PGInMemoryKVService(port, new PGFileDao(Path.of("PGData")));
     }
 }
