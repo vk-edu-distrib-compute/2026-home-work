335971cae9a3261720006fe5ddc49d750b849929
Lillymega <dasha555sibmail.com@gmail.com>
2026-04-12 19:02:37 +0300

    fix KVServiceFactoryArgumentsProvider.java

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
@@ -3,6 +3,7 @@ package company.vk.edu.distrib.compute;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import company.vk.edu.distrib.compute.lillymega.LillymegaKVServiceFactory;
 import company.vk.edu.distrib.compute.aldor7705.KVServiceFactorySimple;
 import company.vk.edu.distrib.compute.andeco.AndecoKVServiceFactory;
 import company.vk.edu.distrib.compute.artttnik.MyKVServiceFactory;
@@ -19,7 +20,6 @@ import company.vk.edu.distrib.compute.vitos23.Vitos23KVServiceFactory;
 import company.vk.edu.distrib.compute.vredakon.VredakonKVServiceFactory;
 import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevKVServiceFactory;
 import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevFileSystemKVServiceFactory;
-
 import company.vk.edu.distrib.compute.luckyslon2003.LuckySlon2003KVServiceFactory;
 import company.vk.edu.distrib.compute.wolfram158.Wolfram158KVServiceFactoryFileWithCacheImpl;
 import org.jspecify.annotations.NonNull;
@@ -30,36 +30,37 @@ import org.junit.jupiter.params.support.ParameterDeclarations;
 import org.junit.platform.commons.util.ReflectionUtils;
 
 public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
-
     private final Set<Class<? extends KVServiceFactory>> factories = Set.of(
-        KVServiceFactorySimple.class,
-        KVServiceFactoryImpl.class,
-        AndecoKVServiceFactory.class,
-        MyKVServiceFactory.class,
-        InMemoryKVServiceFactory.class,
-        Vitos23KVServiceFactory.class,
-        NihuawayKVServiceFactory.class,
-        ShuuuurikFileKVServiceFactory.class,
-        B10nicleKVServiceFactory.class,
-        VredakonKVServiceFactory.class,
-        PopovIgorKVServiceFactoryImpl.class,
-        NesterukiaInMemoryKVServiceFactory.class,
-        NesterukiaFileSystemKVServiceFactory.class,
-        KirillmedvedevKVServiceFactory.class,
-        KirillmedvedevFileSystemKVServiceFactory.class,
-        LuckySlon2003KVServiceFactory.class,
-        Wolfram158KVServiceFactoryFileWithCacheImpl.class,
-        CakeKVServiceFactory.class
+            LillymegaKVServiceFactory.class
+            KVServiceFactorySimple.class,
+            KVServiceFactoryImpl.class,
+            AndecoKVServiceFactory.class,
+            MyKVServiceFactory.class,
+            InMemoryKVServiceFactory.class,
+            Vitos23KVServiceFactory.class,
+            NihuawayKVServiceFactory.class,
+            ShuuuurikFileKVServiceFactory.class,
+            B10nicleKVServiceFactory.class,
+            VredakonKVServiceFactory.class,
+            PopovIgorKVServiceFactoryImpl.class,
+            NesterukiaInMemoryKVServiceFactory.class,
+            NesterukiaFileSystemKVServiceFactory.class,
+            KirillmedvedevKVServiceFactory.class,
+            KirillmedvedevFileSystemKVServiceFactory.class,
+            LuckySlon2003KVServiceFactory.class,
+            Wolfram158KVServiceFactoryFileWithCacheImpl.class,
+            CakeKVServiceFactory.class
+    );
     );
 
     @Override
     @NonNull
     public Stream<? extends Arguments> provideArguments(
-            @NonNull ParameterDeclarations parameters,
-            @NonNull ExtensionContext context
+        @NonNull ParameterDeclarations parameters,
+        @NonNull ExtensionContext context
     ) {
         return factories.stream()
-                .map(ReflectionUtils::newInstance)
-                .map(Arguments::of);
+            .map(ReflectionUtils::newInstance)
+            .map(Arguments::of);
     }
 }
