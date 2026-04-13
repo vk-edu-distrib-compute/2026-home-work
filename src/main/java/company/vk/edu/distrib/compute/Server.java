675a8b5d37127ba57398ed591bbc9d797ae7f20e
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2026-04-13 19:23:04 +0300

    minor fix

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,8 +1,9 @@
 package company.vk.edu.distrib.compute;
 
 import module java.base;
+import company.vk.edu.distrib.compute.dummy.DummyKVClusterFactory;
 import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
-import company.vk.edu.distrib.compute.wedwincode.sharded.EdwinRendezvousKVClusterFactory;
+
 import org.slf4j.LoggerFactory;
 
 public class Server {
@@ -11,7 +12,7 @@ public class Server {
         var log = LoggerFactory.getLogger("server");
         if (isClusterMode(args)) {
             List<Integer> ports = Arrays.asList(8080, 8081);
-            KVCluster cluster = new EdwinRendezvousKVClusterFactory().create(ports); // todo
+            KVCluster cluster = new DummyKVClusterFactory().create(ports);
             cluster.start();
             log.info("Cluster started on ports={}", ports);
             Runtime.getRuntime().addShutdownHook(new Thread(cluster::stop));
