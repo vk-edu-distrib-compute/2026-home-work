d8a297c2376557a8f8a0831f2f18fd44edfd80bc
Глуховский Владислав <vlad.gluhovskiy_18376@mail.ru>
2026-04-13 14:57:31 +0700

    исправление(mandesero): изолировать хранилище FSDao для каждого экземпляра для стабильного CI, как я понял, возникла гонка за использование CI/CD и из-за общего .data

--- src/main/java/company/vk/edu/distrib/compute/mandesero/KVServiceFactoryImpl.java
+++ src/main/java/company/vk/edu/distrib/compute/mandesero/KVServiceFactoryImpl.java
@@ -4,10 +4,12 @@ import company.vk.edu.distrib.compute.KVService;
 import company.vk.edu.distrib.compute.KVServiceFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class KVServiceFactoryImpl extends KVServiceFactory {
     @Override
     protected KVService doCreate(int port) throws IOException {
-        return new KVServiceImpl(port, new FSDao());
+        // Isolated directory per instance; shared default `.data` caused flaky integration tests on CI.
+        return new KVServiceImpl(port, new FSDao(Files.createTempDirectory("mandesero-kv-")));
     }
 }
