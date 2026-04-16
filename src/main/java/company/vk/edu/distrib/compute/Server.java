5eb8601b7990850b38b85e1c02cd9eddbfd36e94
PavelG <pavlusha2003769@yandex.ru>
2026-04-16 23:50:37 +0300

    implement sharding cluster

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,7 +1,7 @@
 package company.vk.edu.distrib.compute;
 
 import module java.base;
-import company.vk.edu.distrib.compute.dummy.DummyKVClusterFactory;
+import company.vk.edu.distrib.compute.golubtsov_pavel.PGClusterFactory;
 import company.vk.edu.distrib.compute.golubtsov_pavel.PGInMemoryKVServiceFactory;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +11,7 @@ public class Server {
         var log = LoggerFactory.getLogger("server");
         if (isClusterMode(args)) {
             List<Integer> ports = Arrays.asList(8080, 8081);
-            KVCluster cluster = new DummyKVClusterFactory().create(ports);
+            KVCluster cluster = new PGClusterFactory().create(ports);
             cluster.start();
             log.info("Cluster started on ports={}", ports);
             Runtime.getRuntime().addShutdownHook(new Thread(cluster::stop));
