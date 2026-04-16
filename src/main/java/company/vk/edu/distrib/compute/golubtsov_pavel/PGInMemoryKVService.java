67ee69a1363c46bdee8ab5894888ce4a892110c0
PavelG <pavlusha2003769@yandex.ru>
2026-04-16 23:50:37 +0300

    implement sharding cluster

--- src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVService.java
+++ src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVService.java
@@ -10,6 +10,11 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
@@ -17,12 +22,21 @@ public class PGInMemoryKVService implements KVService {
     public static final String ID_PARAM_PREFIX = "id=";
     private static final Logger log = LoggerFactory.getLogger(PGInMemoryKVService.class);
     private final Dao<byte[]> dao;
-
     private final HttpServer server;
+    private final String selfEndpoint;
+    private final ShardResolver shardResolver;
+    private final HttpClient httpClient;
 
-    public PGInMemoryKVService(int port, Dao<byte[]> dao) throws IOException {
+    public PGInMemoryKVService(int port,
+                               Dao<byte[]> dao,
+                               String selfEndpoint,
+                               List<String> clusterEndpoints)
+            throws IOException {
         this.server = HttpServer.create(new InetSocketAddress(port), 0);
         this.dao = dao;
+        this.selfEndpoint = selfEndpoint;
+        this.shardResolver = new ShardResolver(clusterEndpoints);
+        this.httpClient = HttpClient.newHttpClient();
         initServer();
     }
 
@@ -40,21 +54,12 @@ public class PGInMemoryKVService implements KVService {
             final var method = http.getRequestMethod();
             final var query = http.getRequestURI().getQuery();
             final var id = parseId(query);
-            if (Objects.equals("GET", method)) {
-                final var value = dao.get(id);
-                http.sendResponseHeaders(200, value.length);
-                http.getResponseBody().write(value);
-            } else if (Objects.equals("PUT", method)) {
-                byte[] body = http.getRequestBody().readAllBytes();
-                dao.upsert(id, body);
-                http.sendResponseHeaders(201, 0);
-            } else if (Objects.equals("DELETE", method)) {
-                dao.delete(id);
-                http.sendResponseHeaders(202, 0);
+            String owner = shardResolver.resolve(id);
+            if (owner.equals(selfEndpoint)) {
+                handleLocalEntityRequest(http, method, id);
             } else {
-                http.sendResponseHeaders(405, 0);
+                proxyRequest(http, owner, id);
             }
-
         }));
     }
 
@@ -62,7 +67,56 @@ public class PGInMemoryKVService implements KVService {
         if ((query == null) || (!query.startsWith(ID_PARAM_PREFIX))) {
             throw new IllegalArgumentException("bad query");
         }
-        return query.substring(ID_PARAM_PREFIX.length());
+        String id = query.substring(ID_PARAM_PREFIX.length());
+        if (id.isBlank()) {
+            throw new IllegalArgumentException("empty id");
+        }
+        return id;
+    }
+
+    private void handleLocalEntityRequest(HttpExchange http, String method, String id) throws IOException {
+        if (Objects.equals("GET", method)) {
+            final var value = dao.get(id);
+            http.sendResponseHeaders(200, value.length);
+            http.getResponseBody().write(value);
+        } else if (Objects.equals("PUT", method)) {
+            byte[] body = http.getRequestBody().readAllBytes();
+            dao.upsert(id, body);
+            http.sendResponseHeaders(201, 0);
+        } else if (Objects.equals("DELETE", method)) {
+            dao.delete(id);
+            http.sendResponseHeaders(202, 0);
+        } else {
+            http.sendResponseHeaders(405, 0);
+        }
+    }
+
+    private void proxyRequest(HttpExchange http, String owner, String id) throws IOException {
+        URI uri = URI.create(owner + "/v0/entity?id=" + id);
+        String method = http.getRequestMethod();
+
+        HttpRequest request;
+        if (Objects.equals("GET", method)) {
+            request = HttpRequest.newBuilder(uri).GET().build();
+        } else if (Objects.equals("DELETE", method)) {
+            request = HttpRequest.newBuilder(uri).DELETE().build();
+        } else if (Objects.equals("PUT", method)) {
+            byte[] body = http.getRequestBody().readAllBytes();
+            request = HttpRequest.newBuilder(uri).PUT(HttpRequest.BodyPublishers.ofByteArray(body)).build();
+        } else {
+            http.sendResponseHeaders(405, 0);
+            return;
+        }
+        try {
+            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            byte[] responseBody = response.body();
+            http.sendResponseHeaders(response.statusCode(), responseBody.length);
+            http.getResponseBody().write(responseBody);
+        } catch (InterruptedException exp) {
+            Thread.currentThread().interrupt();
+            throw new IOException(exp);
+        }
+
     }
 
     @Override
--- src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/golubtsov_pavel/PGInMemoryKVServiceFactory.java
@@ -5,10 +5,16 @@ import company.vk.edu.distrib.compute.KVServiceFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 public class PGInMemoryKVServiceFactory extends KVServiceFactory {
     @Override
     protected KVService doCreate(int port) throws IOException {
-        return new PGInMemoryKVService(port, new PGFileDao(Path.of("PGData")));
+        String selfEndpoint = "http://localhost:" + port;
+        List<String> endpoints = List.of(selfEndpoint);
+        return new PGInMemoryKVService(port,
+                new PGFileDao(Path.of("PGData", String.valueOf(port))),
+                selfEndpoint,
+                endpoints);
     }
 }
