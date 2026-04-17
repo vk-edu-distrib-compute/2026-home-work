cd22a656c0319c71842eb405dd8e5a3f68995a5d
Linempy <mychinnik@yandex.ru>
2026-04-17 20:53:21 +0300

    task_2 - откат

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVClusterFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVClusterFactoryArgumentsProvider.java
@@ -1,6 +1,5 @@
 package company.vk.edu.distrib.compute;
 
-import company.vk.edu.distrib.compute.linempy.scharding.LinempyKVClusterFactory;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,8 +12,7 @@ import java.util.stream.Stream;
 
 public class KVClusterFactoryArgumentsProvider implements ArgumentsProvider {
     private final Set<Class<? extends KVClusterFactory>> factories = Set.of(
-            // DummyKVClusterFactory.class
-            LinempyKVClusterFactory.class
+    //        DummyKVClusterFactory.class
     );
 
     @Override
