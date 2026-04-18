7039311bbc7f049b04d495a4b6fa681bad1b1f1c
artttnik <artttnik@gmail.com>
2026-04-18 17:23:54 +0300

    task2: warn fix

--- src/main/java/company/vk/edu/distrib/compute/artttnik/shard/ShardingStrategy.java
+++ src/main/java/company/vk/edu/distrib/compute/artttnik/shard/ShardingStrategy.java
@@ -2,6 +2,7 @@ package company.vk.edu.distrib.compute.artttnik.shard;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface ShardingStrategy {
     String resolveOwner(String key, List<String> endpoints);
 }
