ec6b67a2bc1b95ecf6858e010948f945c6af040b
Ekaterina Altunina <ekaterina0altunina@gmail.com>
2026-04-19 07:28:13 +0300

    update data

--- mcfluffybottoms-results/task1/get_request.lua
+++ mcfluffybottoms-results/task1/get_request.lua
@@ -3,5 +3,5 @@ wrk.headers["Content-Type"] = "application/json"
 
 request = function()
     local id = math.random(1, 1000000)
-    return wrk.format("GET", "/v0/entity/" .. id, nil)
+    return wrk.format("GET", "http://127.0.0.1:8080/index.html" .. "/v0/entity/" .. id, nil)
 end
--- mcfluffybottoms-results/task1/put_request.lua
+++ mcfluffybottoms-results/task1/put_request.lua
@@ -4,5 +4,5 @@ wrk.headers["Content-Type"] = "application/json"
 request = function()
     local id = math.random(1, 1000000)
     local body = string.format('{"value": "data_%d"}', id)
-    return wrk.format("PUT", "/v0/entity/" .. id, nil, body)
+    return wrk.format("PUT", "http://127.0.0.1:8080/index.html" .. "/v0/entity/" .. id, nil, body)
 end
