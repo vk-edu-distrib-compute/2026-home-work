6b9466ce7a4edbe386763475168d9a5159a0e663
andreylafutkin <andlaf23@gmail.com>
2026-04-20 22:41:39 +0300

    add tests

---
+++ src/main/java/company/vk/edu/distrib/compute/andrey1af/data/task2/get_rendezvous.sh
@@ -0,0 +1,16 @@
+docker run --rm -i \
+  -v "$PWD:/data" \
+  peterevans/vegeta sh -c \
+  "vegeta attack \
+    -format=json \
+    -targets=/data/get_targets.jsonl \
+    -rate=200/s \
+    -duration=30s \
+    -workers=64 \
+    -max-workers=128 \
+    -connections=100 \
+    -max-connections=100 \
+    -keepalive \
+    -http2=false \
+  | tee /data/get_results_rendezvous.bin \
+  | vegeta report -type=hdrplot > /data/get_rendezvous.hgrm"
---
+++ src/main/java/company/vk/edu/distrib/compute/andrey1af/data/task2/put_rendezvous.sh
@@ -0,0 +1,16 @@
+docker run --rm -i \
+  -v "$PWD:/data" \
+  peterevans/vegeta sh -c \
+  "vegeta attack \
+    -format=json \
+    -targets=/data/put_targets.jsonl \
+    -rate=200/s \
+    -duration=30s \
+    -workers=64 \
+    -max-workers=128 \
+    -connections=100 \
+    -max-connections=100 \
+    -keepalive \
+    -http2=false \
+  | tee /data/put_results_rendezvous.bin \
+  | vegeta report -type=hdrplot > /data/put_rendezvous.hgrm"
