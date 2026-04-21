80e000aac9820636315ccc4822b373a119a5e5ff
11qfour <futurenature31@gmail.com>
2026-04-21 21:31:15 +0300

    fix: refactor testbase error with random port

--- src/integrationTest/java/company/vk/edu/distrib/compute/TestBase.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/TestBase.java
@@ -22,9 +22,9 @@ abstract class TestBase {
     public static final Duration TIMEOUT = Duration.ofSeconds(5);
 
     static int randomPort() {
-        final var port = ThreadLocalRandom.current().nextInt(10000, 60000);
         for (int j = 0; j < 5; j++) {
             for (int i = 0; i < 100_000; i++) {
+                final var port = ThreadLocalRandom.current().nextInt(10000, 60000);
                 if (isTcpPortAvailable(port)) {
                     return port;
                 }
