e3dca20db427555d5814f6a81e549663b7e78cb2
Vladislav Guzov <sillmaril@mail.ru>
2026-04-21 12:26:41 +0300

    deleting

--- src/main/java/company/vk/edu/distrib/compute/korjick/wrk/get.lua
+++
@@ -1,6 +0,0 @@
-counter = 0
-
-request = function()
-  counter = counter + 1
-  return wrk.format("GET", "/v0/entity?id=key" .. counter)
-end
--- src/main/java/company/vk/edu/distrib/compute/wolfram158/QueryParamConstants.java
+++
@@ -1,5 +0,0 @@
-package company.vk.edu.distrib.compute.wolfram158;
-
-public class QueryParamConstants {
-    public static final String ID = "id";
-}
