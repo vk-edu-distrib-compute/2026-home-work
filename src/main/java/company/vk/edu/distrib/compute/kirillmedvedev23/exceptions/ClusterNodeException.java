901d9922002846f746b76fbeb3de7928598ba4d7
Unknown-reader <kirillmedvedev23@gmail.com>
2026-04-22 01:03:05 +0300

    VK-DC-2: add serialVersionUID to ClusterNodeException

--- src/main/java/company/vk/edu/distrib/compute/kirillmedvedev23/exceptions/ClusterNodeException.java
+++ src/main/java/company/vk/edu/distrib/compute/kirillmedvedev23/exceptions/ClusterNodeException.java
@@ -1,6 +1,8 @@
 package company.vk.edu.distrib.compute.kirillmedvedev23.exceptions;
 
 public class ClusterNodeException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public ClusterNodeException(String message) {
         super(message);
     }
