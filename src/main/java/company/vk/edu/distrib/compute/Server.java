b0efb1ee81b207a81adc2209a2e70028fd1d3c5c
Yan <Andeco12@yandex.ru>
2026-04-24 20:08:43 +0300

    Server file restored

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -11,13 +11,19 @@ public class Server {
 
     void main(String... args) throws IOException {
         var log = LoggerFactory.getLogger("server");
-
+        if (isClusterMode(args)) {
             List<Integer> ports = Arrays.asList(8080, 8081);
             KVCluster cluster = new AndecoKVClusterFactoryImpl().create(ports);
             cluster.start();
             log.info("Cluster started on ports={}", ports);
             Runtime.getRuntime().addShutdownHook(new Thread(cluster::stop));
-
+        } else {
+            var port = 8080;
+            KVService storage = new DummyKVServiceFactory().create(port);
+            storage.start();
+            log.info("Server started on port {}", port);
+            Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
+        }
     }
 
     private boolean isClusterMode(String... args) {
