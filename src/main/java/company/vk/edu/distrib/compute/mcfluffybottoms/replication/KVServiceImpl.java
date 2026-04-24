41ef578f7790d57f5a3fe1f827e4ee31c4f5c9d4
Ekaterina Altunina <ekaterina0altunina@gmail.com>
2026-04-24 19:44:42 +0300

    add solution

--- src/main/java/company/vk/edu/distrib/compute/mcfluffybottoms/replication/KVServiceImpl.java
+++ src/main/java/company/vk/edu/distrib/compute/mcfluffybottoms/replication/KVServiceImpl.java
@@ -1,27 +1,25 @@
 package company.vk.edu.distrib.compute.mcfluffybottoms.replication;
 
-import java.io.IOException;
-
-import com.sun.net.httpserver.HttpServer;
-
-import company.vk.edu.distrib.compute.KVService;
-import company.vk.edu.distrib.compute.Dao;
-
-import java.net.InetSocketAddress;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.net.httpserver.HttpExchange;
+import company.vk.edu.distrib.compute.ReplicatedService;
+import company.vk.edu.distrib.compute.mcfluffybottoms.replication.ReplicaList.RequestResult;
 
-public class KVServiceImpl implements KVService {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class KVServiceImpl implements ReplicatedService {
     private static final Logger log = LoggerFactory.getLogger(KVServiceImpl.class);
+
     private final HttpServer server;
-    private final Dao<byte[]> dao;
+    private final int port;
+    private final ReplicaList replicas;
 
     private static final String PATH_STATUS = "/v0/status";
     private static final String PATH_ENTITY = "/v0/entity";
@@ -30,9 +28,11 @@ public class KVServiceImpl implements KVService {
     private static final String PUT = "PUT";
     private static final String DELETE = "DELETE";
 
-    public KVServiceImpl(int port, Dao<byte[]> dao) throws IOException {
-        this.dao = dao;
+    public KVServiceImpl(int port) throws IOException {
         this.server = initServer(port);
+        this.port = port;
+        int factor = ReplicationConfigUtils.loadReplicationFactor();
+        this.replicas = new ReplicaList(factor);
     }
 
     private HttpServer initServer(int port) {
@@ -60,6 +60,43 @@ public class KVServiceImpl implements KVService {
         log.info("Stopping");
     }
 
+    @Override
+    public int port() {
+        return this.port;
+    }
+
+    @Override
+    public int numberOfReplicas() {
+        return replicas.numberOfReplicas();
+    }
+
+    @Override
+    public void disableReplica(int nodeId) {
+        replicas.disableReplica(nodeId);
+        log.info("Replica on nodeId {} disabled.", nodeId);
+    }
+
+    @Override
+    public void enableReplica(int nodeId) {
+        try {
+            replicas.enableReplica(nodeId);
+        } catch (IOException e) {
+            log.error("Could not sync replica with nodeId {}.", nodeId, e);
+            return;
+        }
+        log.info("Replica on nodeId {} enabled.", nodeId);
+    }
+
+    private Map<String, String> parseQuery(String query) {
+        Map<String, String> q = new ConcurrentHashMap<>();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf('=');
+            q.put(pair.substring(0, idx), pair.substring(idx + 1));
+        }
+        return q;
+    }
+
     private void handleStatus(HttpExchange exchange) throws IOException {
         try (exchange) {
             if (GET.equals(exchange.getRequestMethod())) {
@@ -73,15 +110,14 @@ public class KVServiceImpl implements KVService {
 
     private void handleEntity(HttpExchange exchange) throws IOException {
         try (exchange) {
-            String method = exchange.getRequestMethod();
             String query = exchange.getRequestURI().getQuery();
             if (query == null) {
                 log.error("Query is empty.");
                 exchange.sendResponseHeaders(400, 0);
                 return;
             }
-
             Map<String, String> args = parseQuery(query);
+
             String id = args.get("id");
             if (id == null || id.isBlank()) {
                 log.error("No id present in the query.");
@@ -89,13 +125,25 @@ public class KVServiceImpl implements KVService {
                 return;
             }
 
+            boolean hasAsk = args.containsKey("ack") && args.get("ack") != null;
+            if (!hasAsk) {
+                log.info("Ack size set to default 1.");
+            }
+            int ack = hasAsk ? Integer.parseInt(args.get("ack")) : 1;
+            if (ack > replicas.numberOfReplicas() || ack < 0) {
+                log.error("Bad ack size {}", ack);
+                exchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            String method = exchange.getRequestMethod();
             switch (method) {
                 case GET ->
-                    handleGet(exchange, id);
+                    handleGet(exchange, id, ack);
                 case PUT ->
-                    handlePut(exchange, id);
+                    handlePut(exchange, id, ack);
                 case DELETE ->
-                    handleDelete(exchange, id);
+                    handleDelete(exchange, id, ack);
                 default ->
                     exchange.sendResponseHeaders(405, 0);
             }
@@ -103,40 +151,49 @@ public class KVServiceImpl implements KVService {
         }
     }
 
-    private void handleGet(HttpExchange exchange, String id) throws IOException {
-        try {
-            byte[] val = dao.get(id);
-            exchange.sendResponseHeaders(200, val.length);
-            exchange.getResponseBody().write(val);
-            log.debug("Got element on id {}.", id);
-        } catch (NoSuchElementException e) {
-            log.error("No element on id {}.", id);
-            exchange.sendResponseHeaders(404, 0);
+    private void handleGet(HttpExchange exchange, String id, int ack) throws IOException {
+        log.info("Get from id {}, need {} replicas to respond.", id, ack);
+
+        RequestResult result = replicas.handleGet(id, ack);
+
+        switch (result.code) {
+            case 500 -> log.warn("Get from id {} failed, responded {}, ack {}", id, result.responded, ack);
+            case 404 -> log.error("No element on id {}.", id);
+            case 200 -> log.debug("Got element on id {}.", id);
+            default -> log.warn("Unexpected status code {} for id {}", result.code, id);
         }
+
+        log.debug("Data got on id {}.", id);
+        result.sendRequest(exchange);
     }
 
-    private void handlePut(HttpExchange exchange, String id) throws IOException {
-        byte[] data = exchange.getRequestBody().readAllBytes();
-        dao.upsert(id, data);
+    private void handlePut(HttpExchange exchange, String id, int ack) throws IOException {
+        log.info("Put from id {}, need {} replicas to respond.", id, ack);
+        byte[] body = exchange.getRequestBody().readAllBytes();
+
+        RequestResult result = replicas.handlePut(body, id, ack);
+
+        switch (result.code) {
+            case 500 -> log.warn("Put in id {} failed, responded {}, ack {}", id, result.responded, ack);
+            case 201 -> log.debug("Got element on id {}.", id);
+            default -> log.warn("Unexpected status code {} for id {}", result.code, id);
+        }
+
         log.debug("Data inserted on id {}.", id);
-        exchange.sendResponseHeaders(201, 0);
+        result.sendRequest(exchange);
     }
 
-    private void handleDelete(HttpExchange exchange, String id) throws IOException {
-        dao.delete(id);
-        log.debug("{} deleted.", id);
-        exchange.sendResponseHeaders(202, 0);
-    }
+    private void handleDelete(HttpExchange exchange, String id, int ack) throws IOException {
+        log.info("Delete from id {}, need {} replicas to respond.", id, ack);
+        RequestResult result = replicas.handleDelete(id, ack);
 
-    private Map<String, String> parseQuery(String query) {
-        Map<String, String> q = new ConcurrentHashMap<>();
-        String[] pairs = query.split("&");
-        for (String pair : pairs) {
-            int idx = pair.indexOf('=');
-            q.put(pair.substring(0, idx), pair.substring(idx + 1));
+        switch (result.code) {
+            case 500 -> log.warn("Delete in id {} failed, responded {}, ack {}", id, result.responded, ack);
+            case 202 -> log.debug("{} deleted.", id);
+            default -> log.warn("Unexpected status code {} for id {}", result.code, id);
         }
-        return q;
-    }
 
-    private void logStats(String logInfo) {}
+        log.debug("Data inserted on id {}.", id);
+        result.sendRequest(exchange);
+    }
 }
---
+++ src/main/java/company/vk/edu/distrib/compute/mcfluffybottoms/replication/ReplicaList.java
@@ -0,0 +1,216 @@
+package company.vk.edu.distrib.compute.mcfluffybottoms.replication;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import company.vk.edu.distrib.compute.Dao;
+import company.vk.edu.distrib.compute.mcfluffybottoms.FileDao;
+
+public class ReplicaList {
+    private final List<Node> nodes;
+    private final Set<String> ids = ConcurrentHashMap.newKeySet();
+    private static final Logger log = LoggerFactory.getLogger(ReplicaList.class);
+
+    class Node {
+        public final Dao<byte[]> dao;
+        AtomicBoolean enabled = new AtomicBoolean(false);
+
+        Node(int i) throws IllegalArgumentException, IOException {
+            this.enabled.set(true);
+            this.dao = new FileDao(Path.of("node-" + i));
+        }
+
+        public void disable() {
+            this.enabled.set(false);
+        }
+
+        public void enable() {
+            this.enabled.set(true);
+        }
+
+        public boolean enabled() {
+            return this.enabled.get();
+        }
+    }
+
+    class RequestResult {
+        public final int code;
+        public final byte[] value;
+        public final int responded;
+
+        public RequestResult(int code, byte[] value, int responded) {
+            this.code = code;
+            this.value = value == null ? null : value.clone();
+            this.responded = responded;
+        }
+
+        public void sendRequest(HttpExchange exchange) throws IOException {
+            exchange.sendResponseHeaders(this.code, this.value.length);
+            exchange.getResponseBody().write(this.value);
+        }
+    }
+
+    public ReplicaList(int factor) throws IllegalArgumentException, IOException {
+        this.nodes = initNodes(factor);
+    }
+
+    private List<Node> initNodes(int factor) throws IllegalArgumentException, IOException {
+        List<Node> nodes = new ArrayList<>();
+        for (int i = 0; i < factor; ++i) {
+            nodes.add(new Node(i));
+        }
+        return nodes;
+    }
+
+    public int numberOfReplicas() {
+        return nodes.size();
+    }
+
+    public void disableReplica(int nodeId) {
+        Node node = tryGet(nodeId);
+        node.disable();
+    }
+
+    public void enableReplica(int nodeId) throws IllegalArgumentException, IOException {
+        Node node = tryGet(nodeId);
+        syncNodes(node);
+        node.enable();
+    }
+
+    private Node tryGet(int nodeId) {
+        Node node = nodes.get(nodeId);
+        if (node == null) {
+            throw new NoSuchElementException("nodeId '" + nodeId + "' not found.");
+        }
+        return node;
+    }
+
+    private List<Node> getNodeOrderById(String id) {
+        int start = Math.abs(id.hashCode()) % nodes.size();
+        List<Node> finalOrder = new ArrayList<>();
+
+        for (int i = 0; i < nodes.size(); i++) {
+            finalOrder.add(nodes.get((start + i) % nodes.size()));
+        }
+        return finalOrder;
+    }
+
+    private void syncNodes(Node toSync) throws IllegalArgumentException, IOException {
+        for (String id : ids) {
+            syncNode(toSync, id);
+        }
+    }
+
+    private void syncNode(Node toSync, String id) throws IllegalArgumentException, IOException {
+        byte[] val = null;
+        for (Node node : getNodeOrderById(id)) {
+            if (!node.enabled() || node == toSync) {
+                continue;
+            }
+
+            try {
+                val = node.dao.get(id);
+                if (val != null) {
+                    break;
+                }
+            } catch (NoSuchElementException e) {
+                toSync.dao.delete(id);
+            }
+        }
+
+        if (val != null) {
+            toSync.dao.upsert(id, val);
+        } else {
+            toSync.dao.delete(id);
+        }
+    }
+
+    public RequestResult handleGet(String id, int ack) throws IOException {
+        int responded = 0;
+        byte[] val = null;
+
+        for (Node node : getNodeOrderById(id)) {
+            if (!node.enabled()) {
+                continue;
+            }
+
+            try {
+                byte[] v = node.dao.get(id);
+                if (v != null) {
+                    val = v;
+                }
+                responded++;
+            } catch (NoSuchElementException e) {
+                responded++;
+            }
+        }
+
+        if (responded < ack) {
+            return new RequestResult(500, new byte[0], responded);
+        }
+
+        if (val == null) {
+            return new RequestResult(404, new byte[0], responded);
+        } else {
+            return new RequestResult(200, val, responded);
+        }
+    }
+
+    public RequestResult handlePut(byte[] body, String id, int ack) throws IOException {
+        int responded = 0;
+
+        for (Node node : getNodeOrderById(id)) {
+            if (!node.enabled()) {
+                continue;
+            }
+
+            try {
+                node.dao.upsert(id, body);
+                responded++;
+            } catch (NoSuchElementException e) {
+                log.warn("Failed to upsert id {} on node: {}", id, e.getMessage());
+            }
+        }
+
+        ids.add(id);
+        if (responded < ack) {
+            return new RequestResult(500, new byte[0], responded);
+        }
+
+        return new RequestResult(201, new byte[0], responded);
+    }
+
+    public RequestResult handleDelete(String id, int ack) throws IOException {
+        int responded = 0;
+
+        for (Node node : getNodeOrderById(id)) {
+            if (!node.enabled()) {
+                continue;
+            }
+
+            try {
+                node.dao.delete(id);
+                responded++;
+            } catch (NoSuchElementException e) {
+                log.warn("Failed to delete id {} on node: {}", id, e.getMessage());
+            }
+        }
+
+        if (responded < ack) {
+            return new RequestResult(500, new byte[0], responded);
+        }
+
+        return new RequestResult(202, new byte[0], responded);
+    }
+}
