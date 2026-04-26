b9748f2a249eb636c0035307dd98d83597621985
Denis Sokolov <denchika02@yandex.ru>
2026-04-26 13:29:57 +0300

    init hw

---
+++ src/main/java/company/vk/edu/distrib/compute/denchika/dao/BaseDao.java
@@ -0,0 +1,12 @@
+package company.vk.edu.distrib.compute.denchika.dao;
+
+public abstract class BaseDao {
+
+    protected static final int MAX_SIZE = 1024 * 1024;
+
+    protected static void validateKey(String key) {
+        if (key == null || key.isBlank()) {
+            throw new IllegalArgumentException("Invalid key");
+        }
+    }
+}
