d830fd4603f4ec7b66b78c3969e84c49a1ecce33
Ronshin-Vsevolod <vronshin@gmail.com>
2026-04-27 21:40:00 +0300

    basic test

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
@@ -6,21 +6,19 @@ import java.util.stream.Stream;
 import company.vk.edu.distrib.compute.aldor7705.KVServiceFactorySimple;
 import company.vk.edu.distrib.compute.andeco.AndecoKVServiceFactory;
 import company.vk.edu.distrib.compute.artttnik.MyKVServiceFactory;
-import company.vk.edu.distrib.compute.ip.PopovIgorKVServiceFactoryImpl;
-import company.vk.edu.distrib.compute.gavrilova_ekaterina.InMemoryKVServiceFactory;
 import company.vk.edu.distrib.compute.b10nicle.B10nicleKVServiceFactory;
+import company.vk.edu.distrib.compute.gavrilova_ekaterina.InMemoryKVServiceFactory;
+import company.vk.edu.distrib.compute.ip.PopovIgorKVServiceFactoryImpl;
+import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevFileSystemKVServiceFactory;
+import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevKVServiceFactory;
 import company.vk.edu.distrib.compute.korjick.CakeKVServiceFactory;
-import company.vk.edu.distrib.compute.nesterukia.file_system.NesterukiaFileSystemKVServiceFactory;
-import company.vk.edu.distrib.compute.nesterukia.in_memory.NesterukiaInMemoryKVServiceFactory;
-import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVServiceFactory;
+import company.vk.edu.distrib.compute.luckyslon2003.LuckySlon2003KVServiceFactory;
 import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
+import company.vk.edu.distrib.compute.nesterukia.file_system.NesterukiaFileSystemKVServiceFactory;
+import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVServiceFactory;
 import company.vk.edu.distrib.compute.shuuuurik.ShuuuurikFileKVServiceFactory;
 import company.vk.edu.distrib.compute.vitos23.Vitos23KVServiceFactory;
 import company.vk.edu.distrib.compute.vredakon.VredakonKVServiceFactory;
-import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevKVServiceFactory;
-import company.vk.edu.distrib.compute.kirillmedvedev23.KirillmedvedevFileSystemKVServiceFactory;
-
-import company.vk.edu.distrib.compute.luckyslon2003.LuckySlon2003KVServiceFactory;
 import company.vk.edu.distrib.compute.wolfram158.Wolfram158KVServiceFactoryFileWithCacheImpl;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -28,14 +26,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.support.ParameterDeclarations;
 import org.junit.platform.commons.util.ReflectionUtils;
-
-import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
-import company.vk.edu.distrib.compute.gavrilova_ekaterina.InMemoryKVServiceFactory;
-import company.vk.edu.distrib.compute.b10nicle.B10nicleKVServiceFactory;
-import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVServiceFactory;
-import company.vk.edu.distrib.compute.vitos23.Vitos23KVServiceFactory;
-import company.vk.edu.distrib.compute.ronshinvsevolod.RonshinInMemoryKVServiceFactory;
-import company.vk.edu.distrib.compute.ronshinvsevolod.RonshinFileKVServiceFactory;
 
 public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
 
@@ -45,15 +35,12 @@ public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
         AndecoKVServiceFactory.class,
         MyKVServiceFactory.class,
         InMemoryKVServiceFactory.class,
+        Vitos23KVServiceFactory.class,
+        NihuawayKVServiceFactory.class,
         ShuuuurikFileKVServiceFactory.class,
         B10nicleKVServiceFactory.class,
-        NihuawayKVServiceFactory.class,
-        Vitos23KVServiceFactory.class,
-        RonshinInMemoryKVServiceFactory.class,
-        RonshinFileKVServiceFactory.class,
         VredakonKVServiceFactory.class,
         PopovIgorKVServiceFactoryImpl.class,
-        NesterukiaInMemoryKVServiceFactory.class,
         NesterukiaFileSystemKVServiceFactory.class,
         KirillmedvedevKVServiceFactory.class,
         KirillmedvedevFileSystemKVServiceFactory.class,
