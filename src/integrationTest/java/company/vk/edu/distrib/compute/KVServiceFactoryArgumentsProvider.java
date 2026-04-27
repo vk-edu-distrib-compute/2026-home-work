75378cf0676958fe4de0e0cbab935986d5b092ba
PavelG <pavlusha2003769@yandex.ru>
2026-04-27 21:12:12 +0300

    fix CI tests

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
@@ -3,18 +3,7 @@ package company.vk.edu.distrib.compute;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import company.vk.edu.distrib.compute.aldor7705.KVServiceFactorySimple;
-import company.vk.edu.distrib.compute.andeco.AndecoKVServiceFactory;
-import company.vk.edu.distrib.compute.artttnik.MyKVServiceFactory;
-import company.vk.edu.distrib.compute.ip.PopovIgorKVServiceFactoryImpl;
-import company.vk.edu.distrib.compute.gavrilova_ekaterina.InMemoryKVServiceFactory;
-import company.vk.edu.distrib.compute.b10nicle.B10nicleKVServiceFactory;
-import company.vk.edu.distrib.compute.nesterukia.file_system.NesterukiaFileSystemKVServiceFactory;
-import company.vk.edu.distrib.compute.nesterukia.in_memory.NesterukiaInMemoryKVServiceFactory;
-import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVServiceFactory;
-import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
-import company.vk.edu.distrib.compute.vitos23.Vitos23KVServiceFactory;
-import company.vk.edu.distrib.compute.vredakon.VredakonKVServiceFactory;
+import company.vk.edu.distrib.compute.golubtsov_pavel.PGInMemoryKVServiceFactory;
 
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -26,18 +15,7 @@ import org.junit.platform.commons.util.ReflectionUtils;
 public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
 
     private final Set<Class<? extends KVServiceFactory>> factories = Set.of(
-        KVServiceFactorySimple.class,
-        KVServiceFactoryImpl.class,
-        AndecoKVServiceFactory.class,
-        MyKVServiceFactory.class,
-        InMemoryKVServiceFactory.class,
-        Vitos23KVServiceFactory.class,
-        NihuawayKVServiceFactory.class,
-        B10nicleKVServiceFactory.class,
-        VredakonKVServiceFactory.class,
-        PopovIgorKVServiceFactoryImpl.class,
-        NesterukiaInMemoryKVServiceFactory.class,
-        NesterukiaFileSystemKVServiceFactory.class
+        PGInMemoryKVServiceFactory.class
     );
 
     @Override
